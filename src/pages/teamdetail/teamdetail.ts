211515915adbb5c66e8e352cb6b464005a5a95d5
604234057cs <604234057@paichat.skru.ac.th>
2019-02-11 00:22:34 +0700

    loveteam

--- src/pages/teamdetail/teamdetail.ts
+++ src/pages/teamdetail/teamdetail.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Friend } from '../../Models/teammodels';
+import { TeamrestProvider } from '../../providers/teamrest/teamrest';
 
 /**
  * Generated class for the TeamdetailPage page.
@@ -13,12 +15,26 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'teamdetail.html',
 })
 export class TeamdetailPage {
+  studentID:string;
+  friend:Friend;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public teamrest:TeamrestProvider,public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TeamdetailPage');
+  }
+  ionViewWillEnter(){
+    console.log('ionViewWillEnter');
+    this.studentID=this.navParams.get("studentID");
+    console.log(this.studentID);
+    this.teamrest.getFriendsList().subscribe( data =>{
+      this.friend=data.filter(friend=> friend.studentID === this.studentID)[0];
+      }
+    );
+  }
+  goBack(){
+    this.navCtrl.pop();
   }
 
 }
